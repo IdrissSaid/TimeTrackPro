e180a822c79a1820cbd9de5735a1ce34662caf19
idriss <idriss.said@epitech.eu>
2024-02-07 21:35:23 +0400

    Update user

--- website/app/api/user/[id]/route.ts
+++ website/app/api/user/[id]/route.ts
@@ -34,3 +34,39 @@ export async function DELETE(request: Request,{ params }: { params: { id: string
     return NextResponse.json({message: err}, {status: 500})
   }
 }
+
+export async function PUT(request: Request,{ params }: { params: { id: string } }) {
+  if (!params.id) {
+    return NextResponse.json({status: 400})
+  }
+  await connectDB()
+
+  try {
+    const { firstName, lastName, role } = await request.json()
+    const userFound = await user?.findOne({ code: params.id }).select('firstName role lastName code')
+    if (!userFound) {
+      return NextResponse.json({messge: "User not found"}, {status: 404})
+    }
+    if (firstName) userFound.firstName = firstName
+    if (lastName) userFound.lastName = lastName
+    if (role && typeof role === 'string') {
+      const index = userFound.role.indexOf(role);
+      userFound.role = index === -1
+        ? [...userFound.role, role]
+        : userFound.role.filter(item => item !== role);
+    } else if (role && Array.isArray(role) && role.length > 0) {
+      role.forEach((roleItem) => {
+        const index = userFound.role.indexOf(roleItem);
+        userFound.role = index === -1
+          ? [...userFound.role, roleItem]
+          : userFound.role.filter(item => item !== roleItem);
+        });
+      }
+    if (userFound.role.length == 0)
+      userFound.role.push("USER")
+    userFound.save()
+    return NextResponse.json({message: "User Modified"})
+  } catch (err) {
+    return NextResponse.json({message: err}, {status: 500})
+  }
+}
